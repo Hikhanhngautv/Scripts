b0afa72c3e1e0164a700bd64c35a173d061bb00b
Hikhanhngautv <119961760+Hikhanhngautv@users.noreply.github.com>
2025-08-30 02:09:13 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,20 @@
 -- Load thư viện Rayfield (bản của bạn)
 local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/Hikhanhngautv/Rayfield-backup/main/Rayfield'))()
+
+-- Tạo cửa sổ chính
+local Window = Rayfield:CreateWindow({
+    Name = "🍌 Banana Hamster Hub - Blox Fruits [ Premium ] by Hikhanhngautv",
+    LoadingTitle = "Banana Hamster GUI",
+    LoadingSubtitle = "by Hikhanhngautv",
+    ConfigurationSaving = {
+        Enabled = true,
+        FolderName = "BananaHamsterHub", -- thư mục lưu config
+        FileName = "BananaHamsterConfig" -- tên file config
+    },
+    Discord = {
+        Enabled = false,
+        Invite = "discord.gg/xxxx", -- bỏ trống nếu không có
+        RememberJoins = true
+    },
+    KeySystem = false -- đổi thành true nếu bạn muốn có hệ thống key
+})
